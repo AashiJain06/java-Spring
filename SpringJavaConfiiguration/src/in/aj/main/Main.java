8d0eed94c81625452f6aad7074592a8896dd2535
Aashi Jain <aashijain064@gmail.com>
2025-06-17 13:04:44 +0530

    Add files via upload

--- SpringJavaConfiiguration/src/in/aj/main/Main.java
+++ SpringJavaConfiiguration/src/in/aj/main/Main.java
@@ -1,14 +1,15 @@
 package in.aj.main;
-
+import in.aj.resources.JavaConfig;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import in.aj.beans.Student;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 
 public class Main {
 	public static void main (String[] args)
 	{
-	ApplicationContext context =new AnnotationConfigApplicationContext("JavaConfig.class");
-	Student std = (Student)context.getBean("studId");
+	ApplicationContext context =new AnnotationConfigApplicationContext(JavaConfig.class);
+	Student std = (Student)context.getBean(Student.class);
 	std.display1();
 	}
 
--- SpringJavaConfiiguration/src/in/aj/resources/JavaConfig.java
+++ SpringJavaConfiiguration/src/in/aj/resources/JavaConfig.java
@@ -1,17 +1,16 @@
 package in.aj.resources;
-
+import in.aj.beans.Student;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import in.aj.beans.Student;
 
 @Configuration
 public class JavaConfig {
-	@Bean("studId")
-	public Student obj1()
+	@Bean(name="aashi")
+	public Student student()
 	{
 		Student st = new Student();
-		st.setCity("Indore");
+	    st.setCity("Indore");
 		st.setName("Aashi");
 		st.setRoll(101);
 		return st;
