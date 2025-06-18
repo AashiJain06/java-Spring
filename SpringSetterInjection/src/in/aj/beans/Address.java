76c0d3ded07fc1a43b7e240143c9d788495a5ab9
Aashi Jain <aashijain064@gmail.com>
2025-06-18 13:22:37 +0530

    Add files via upload

---
+++ SpringSetterInjection/src/in/aj/beans/Address.java
@@ -0,0 +1,26 @@
+package in.aj.beans;
+
+public class Address {
+	private String city;
+	private int pin;
+	private int house;
+	
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setPin(int pin) {
+		this.pin = pin;
+	}
+	public void setHouse(int house) {
+		this.house = house;
+	}
+	@Override
+	public String toString() {
+		
+		return "#"+city+", "+pin+", "+house;
+	}
+
+}
